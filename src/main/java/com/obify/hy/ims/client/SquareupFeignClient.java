b3733acc5dd8ac112030ddfe3c98c4a04f779087
Ranjan <ranjanpandey2006@gmail.com>
2024-09-20 03:11:38 +0530

    feign integration

--- src/main/java/com/obify/hy/ims/client/SquareupFeignClient.java
+++ src/main/java/com/obify/hy/ims/client/SquareupFeignClient.java
@@ -1,14 +1,20 @@
 package com.obify.hy.ims.client;
 
 import com.obify.hy.ims.client.model.LocationModelWrapper;
+import com.obify.hy.ims.client.model.SalesModelWrapper;
+import com.obify.hy.ims.client.model.SalesRequestModel;
 import com.obify.hy.ims.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "squareupFeignClient", url = "${squareup.url}", configuration = FeignClientConfiguration.class)
 public interface SquareupFeignClient {
 
     @GetMapping("/locations")
     ResponseEntity<LocationModelWrapper> getAllLocations();
+    @PostMapping("/orders/search")
+    ResponseEntity<SalesModelWrapper> getFilteredSales(@RequestBody SalesRequestModel model);
 }
--- src/main/java/com/obify/hy/ims/controller/PostController.java
+++ src/main/java/com/obify/hy/ims/controller/PostController.java
@@ -2,12 +2,13 @@ package com.obify.hy.ims.controller;
 
 import com.obify.hy.ims.client.JSONPlaceholderFeign;
 import com.obify.hy.ims.client.SquareupFeignClient;
-import com.obify.hy.ims.client.model.LocationModelWrapper;
+import com.obify.hy.ims.client.model.*;
 import com.obify.hy.ims.dto.PostDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -33,5 +34,36 @@ public class PostController {
     @GetMapping("/locations")
     ResponseEntity<LocationModelWrapper> getAllLocations(){
        return squareupFeignClient.getAllLocations();
+    }
+
+    @GetMapping("/sales")
+    ResponseEntity<SalesModelWrapper> getAllSales(){
+
+        SalesQueryStateFilter sqsf = new SalesQueryStateFilter();
+        sqsf.setStates(List.of("COMPLETED"));
+
+        SalesQueryRequestModel sqrm = new SalesQueryRequestModel();
+
+        StartAtModel sam = new StartAtModel();
+        sam.setStart_at(LocalDateTime.now().toString());
+
+        ClosedAtFilter caf = new ClosedAtFilter();
+        //caf.setStart_at(sam);
+
+        SalesDateTimeFilter sdtf = new SalesDateTimeFilter();
+        //sdtf.setClosed_at(caf);
+
+        SalesQueryFilterModel sqfm = new SalesQueryFilterModel();
+        //sqfm.setDate_time_filter(sdtf);
+        sqfm.setState_filter(sqsf);
+
+        sqrm.setFilter(sqfm);
+
+        SalesRequestModel sqm = new SalesRequestModel();
+        sqm.setReturn_entries(true);
+        sqm.setLocation_ids(List.of("LNM38YF22M4V0"));
+        sqm.setQuery(sqrm);
+
+        return squareupFeignClient.getFilteredSales(sqm);
     }
 }
